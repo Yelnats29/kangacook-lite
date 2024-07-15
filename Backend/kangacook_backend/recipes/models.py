8d47d7ac640fdd42b8c3bb8dd1a1d048d7d6595c
Yelnats <s.jeanjr@gmail.com>
2024-07-15 12:24:56 -0400

    addedand completed serializers file

--- Backend/kangacook_backend/recipes/models.py
+++ Backend/kangacook_backend/recipes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    ingredients = models.TextField()
+    steps = models.TextField()
+
+    def __str__(self):
+        return self.name
