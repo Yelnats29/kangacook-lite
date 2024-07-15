08853962cabe381d40d20c6780c2c306c1fb8b4a
Yelnats <s.jeanjr@gmail.com>
2024-07-14 22:09:17 -0400

    created the routes on main

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+    </Router>
   </React.StrictMode>,
-)
+);
