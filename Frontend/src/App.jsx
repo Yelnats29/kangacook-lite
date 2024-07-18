b63d477d35a0deb680e36c73a6e969fef831910e
Yelnats <s.jeanjr@gmail.com>
2024-07-17 21:32:43 -0400

    corrected imports on App.jsx page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './HomePage';
+import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
 import CreateRecipe from './components/CreateRecipe';
 import UpdateRecipe from './components/UpdateRecipe';
-import DeleteRecipe from './components/DeleteRecipe';
 
 // The App component renders the HomePage component as the default route. It also defines the routes for the other components.
 const App = () => {
