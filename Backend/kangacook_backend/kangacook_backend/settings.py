397f852bf1d3a6663f285d11afe07e8adc4c58fe
Yelnats <s.jeanjr@gmail.com>
2024-07-19 02:23:53 -0400

    changed IP

--- Backend/kangacook_backend/kangacook_backend/settings.py
+++ Backend/kangacook_backend/kangacook_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hz=syqcshhwpbf0rbw0!%a&y-gw$(y7#lbyj8+&jyh(nh%p)%x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.196.232.39', 'localhost']
+ALLOWED_HOSTS = ['54.163.21.225', 'localhost']
 
 
 # Application definition
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
