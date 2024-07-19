6eecb09ffbe34b6a58574f057cb5c14bea790621
Yelnats <s.jeanjr@gmail.com>
2024-07-18 23:38:42 -0400

    deleted rest framework

--- Backend/kangacook_backend/kangacook_backend/settings.py
+++ Backend/kangacook_backend/kangacook_backend/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
     'recipes',
     'django.contrib.admin',
     'django.contrib.auth',
